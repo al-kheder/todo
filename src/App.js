040f4f1721913f8ca88d38f732035bab650f4f81
Ali <alkhdyyr@gmail.com>
2023-08-08 11:55:50 +0200

    todo react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import {React, useState} from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import Todo from './components/Todo';
 
 function App() {
+  let [todos, setTodos] =useState([]);
+  const [todoToShow,setTodoToShow] =useState("all");
+  const [toggleAllComplete,setToggleAllComplete] = useState(true); 
+  const addTodo = (todo) => {
+    setTodos([todo, ...todos]);
+  }
+  const handleDelete =  (id) => {
+    setTodos(todos.filter( (todo) => todo.id !== id))
+  };
+  const updateTodoToshow =  (s) => {
+    setTodoToShow(s);
+  }
+  const removeAllTodosThatAreComplet =  (s) => {
+    setTodos(todos.filter( (todo) => !todo.comlete));
+  } 
+  const toggleComplete =  (id) => {
+    setTodos(todos.map( (todo) => {
+       if(todo.id ===id){
+        return{
+          ...todo,
+          complete: !todo.complete
+        }
+        }else{
+          return todo;
+        }
+    }))  
+  }
+   if(todoToShow ==="active"){
+    todos = todos.filter((todo)=> !todo.complete)
+    }else  if(todoToShow ==="complete"){ 
+      todos = todos.filter((todo)=> todo.complete)
+    }
+      
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <div className='container'>
+     <TodoForm onSubmit = {addTodo}/>  
+     {
+      todos.map( (todo) => 
+         ( 
+         <Todo key={todo.id}
+          todo ={todo}
+          onDelete = {() => handleDelete(todo.id)} 
+          toggleComplete = { () => toggleComplete(todo.id)}
+          />
+         )
+      )
+     }
+     <div>
+      <button className='update-btn btn'onClick={ () => updateTodoToshow("all")}>All</button>
+      <button className='update-btn btn'onClick={ () => updateTodoToshow("active")}>Active</button>
+      <button className='update-btn btn'onClick={ () => updateTodoToshow("complete")}>Complete</button>
+     </div>
+
+    {todos.some( (todo) => todo.complete)?
+    (<button className='all-btn btn' onClick={removeAllTodosThatAreComplet}>Remove all complete todos</button>) : null }
+     <button className='all-btn btn' onClick={ () => {
+      setTodos(
+        todos.map( (todo) =>({
+          ...todo,
+          complete :toggleAllComplete,
+ } ))
+      );
+      setToggleAllComplete(!toggleAllComplete);
+     }}>Toggle all complete :{`${toggleAllComplete}`} </button>
+  </div>
   );
 }
 
